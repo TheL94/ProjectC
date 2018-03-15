87101144801df202a9e5ba0a18e457453691fe87
Luca Garbelli <lu.gbelli@hotmail.it>
2018-03-15 13:39:41 +0100

    Aggiunto il parse dell'input

---
+++ ShortestPathToWork/ShortestPathToWork/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPathToWork
+{
+    public class Point
+    {
+        public Polygonal PolygonalOfAppartenence;
+        public int X;
+        public int Y;
+
+        public Point() { }
+
+        public Point(int _x, int _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public Point(int _x, int _y, Polygonal _polygonalOfAppartenence)
+        {
+            X = _x;
+            Y = _y;
+            PolygonalOfAppartenence = _polygonalOfAppartenence;
+        }
+    }
+}
