207ed7a3445d2548b47702fc44eb994dd592218e
FlyingBoar <montecristo310@gmail.com>
2018-03-15 19:49:04 +0100

    no message

---
+++ ShortestPathToWork/ChallengeProject/Evaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeProject
+{
+    public static class Evaluator
+    {
+
+
+    }
+
+    public class EvaluatedData
+    {
+        public string ProviderName;
+        public string RegionName;
+        public int Units;
+    }
+}
