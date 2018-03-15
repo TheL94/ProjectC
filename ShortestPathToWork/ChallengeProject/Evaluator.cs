2abbfde5f2a9f26218cb93d66182ba02e74843f6
FlyingBoar <montecristo310@gmail.com>
2018-03-15 20:22:36 +0100

    sdavasfd

--- ShortestPathToWork/ChallengeProject/Evaluator.cs
+++ ShortestPathToWork/ChallengeProject/Evaluator.cs
@@ -8,7 +8,47 @@ namespace ChallengeProject
 {
     public static class Evaluator
     {
+        public static List<EvaluatedData> EvaluateProject(Project _project, int _maxCost, int _minCost, ParsedData _data)
+        {
+            List<EvaluatedData> returnData = new List<EvaluatedData>();
+            float actualK = (float)Math.Pow(_project.PenaltyValue / _minCost, _minCost / _maxCost);
 
+            ServicePerUnit[] allocated;
+
+            foreach (EvaluatedData item in returnData)
+            {
+                item.Units
+            }
+        }
+
+        static EvaluatedData SerchBestUnit(ServicePerUnit[] _allocated, ServicePerUnit[] _needed, ParsedData _data, float _actualK, Project project)
+        {
+            ServicePerUnit[] actualBestAllocation = new ServicePerUnit[_data.Services.Length];
+            float param = -1;
+
+            foreach (var provider in _data.Providers)
+            {
+                foreach (var region in provider.regions)
+                {
+                    for (int i = 0; i < region.Units.Services.Length; i++)
+                    {
+                        actualBestAllocation[i].Quantity = _allocated[i].Quantity + region.Units.Services[i].Quantity;
+
+                    }
+
+                    float dinstance = 0;
+
+                    for (int i = 0; i < region.Units.Services.Length; i++)
+                    {
+                        dinstance += (float)Math.Pow((_needed[i].Quantity - actualBestAllocation[i].Quantity), 2);
+                    }
+                    if(param == -1)
+                    {
+                        param = dinstance * (float)Math.Pow(region.CostPerUnit, _actualK) * region.RegionLatency[project.Country]
+                    }
+                }
+            }
+        }
 
     }
 
