807665ffe94b6f87bc64ebe8d09a263cb74cefee
Luca Garbelli <lu.gbelli@hotmail.it>
2018-03-15 12:20:19 +0100

    Stuttura base del programma

--- ShortestPathToWork/ShortestPathToWork/Program.cs
+++ ShortestPathToWork/ShortestPathToWork/Program.cs
@@ -10,7 +10,34 @@ namespace ShortestPathToWork
     {
         static void Main(string[] args)
         {
+            InitApplication(Environment.SpecialFolder.Desktop);
 
+            // passare il parh alla classe di lettura
+            
+            // prendere il ritorno del reader e passarlo al pathfinder
+
+            // scrivere su nuovo file il ritorno del pathfinder formattato
+
+            ExitApplication();
+        }
+
+        static string InitApplication(Environment.SpecialFolder _folderToLook)
+        {
+            Console.WriteLine("Insert File Name With Format :");
+            string fileName = Console.ReadLine();
+
+            string path = Environment.GetFolderPath(_folderToLook);
+            path += "\\" + fileName;
+
+            return path;
+        }
+
+        static void ExitApplication()
+        {
+            Console.WriteLine("Press ESC to terminate.");
+            ConsoleKeyInfo pressedKey = Console.ReadKey();
+            if (pressedKey.Key == ConsoleKey.Escape)
+                return;
         }
     }
 }
