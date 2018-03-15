2dab8453b7eea7c4bb660f7bd7bd927ce324dda8
Luca Garbelli <lu.gbelli@hotmail.it>
2018-03-15 16:57:55 +0100

    Preparato inizio progetto

---
+++ ShortestPathToWork/ChallengeProject/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeProject
+{
+    public class Parser
+    {
+        public static string[][] ParseInputputData(string[][] _arraysToParse)
+        {
+            return null;
+        }
+
+        public static string[][] ParseOutputData()
+        {
+            return null;
+        }
+    }
+}
--- ShortestPathToWork/ChallengeProject/Program.cs
+++ ShortestPathToWork/ChallengeProject/Program.cs
@@ -8,9 +8,24 @@ namespace ChallengeProject
 {
     class Program
     {
+        static string EnvironmentPath { get { return Environment.GetFolderPath(Environment.SpecialFolder.Desktop); } }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Insert File Name With Format :");
 
+            string fileName = Console.ReadLine();
+            string path = EnvironmentPath + "\\" + fileName; ;
+
+            string[][] inputText = FileManager.Read(path, ' ');
+
+
+            Console.WriteLine("Data Writed in output.txt");
+            Console.WriteLine("Press ESC to terminate.");
+
+            ConsoleKeyInfo pressedKey = Console.ReadKey();
+            if (pressedKey.Key == ConsoleKey.Escape)
+                return;
         }
     }
 }
